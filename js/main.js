7c507a79cffff03d086da58bc6b8d79ba776af59
AlexandraRothenberg <alexandra.rothenberg12@gmail.com>
2016-03-22 01:38:13 -0400

    files added and html colour button made

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var $ball = $('.ball');
+
+
+//$('html').on('keydown', function (e) {
+//  var $ball = $('<div>');
+//  var newArea = (Math.random() * (1000 - 10)) + 10;
+//  var newThing = (Math.random() * (1000 - 10)) + 10;
+
+//  $ball.addClass('ball');
+//  $ball.css('left', newArea);
+//  $ball.css('top', newThing);
+
+//  e.preventDefault();
+//  $('body').append($ball);
+//});
